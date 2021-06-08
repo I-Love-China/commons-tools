02ffbc0de6086e0db5e78dcc87d64232abc3397a
typist <z482728291@gmail.com>
2021-06-08 21:46:31 +0800

    Arch:MapUtils:删除丢人现眼的 MapUtils
    可用 TreeMap 实现

--- commons-collection/src/main/java/top/laitne/collection/MapUtils.java
+++ commons-collection/src/main/java/top/laitne/collection/MapUtils.java
@@ -1,9 +1,6 @@
 package top.laitne.collection;
 
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -15,41 +12,6 @@ public final class MapUtils {
     public static boolean isEmpty(Map map) {
         return null == map || map.size() == 0;
     }
-
-    public static <K extends Comparable<K>, V> LinkedHashMap<K, V> sort(Map<K, V> map) {
-        LinkedHashMap<K, V> linkedHashMap = new LinkedHashMap<>();
-        if (isEmpty(map)) {
-            return linkedHashMap;
-        }
-
-        List<Map.Entry<K, V>> entries = map.entrySet().stream()
-                .sorted(Comparator.comparing(Map.Entry::getKey))
-                .collect(Collectors.toList());
-
-        for (Map.Entry<K, V> entry : entries) {
-            linkedHashMap.put(entry.getKey(), entry.getValue());
-        }
-
-        return linkedHashMap;
-    }
-
-    public static <K, V> LinkedHashMap<K, V> sort(Map<K, V> map, Comparator<K> comparator) {
-        LinkedHashMap<K, V> linkedHashMap = new LinkedHashMap<>();
-        if (isEmpty(map)) {
-            return linkedHashMap;
-        }
-
-        List<Map.Entry<K, V>> entries = map.entrySet().stream()
-                .sorted((a, b) -> comparator.compare(a.getKey(), b.getKey()))
-                .collect(Collectors.toList());
-
-        for (Map.Entry<K, V> entry : entries) {
-            linkedHashMap.put(entry.getKey(), entry.getValue());
-        }
-
-        return linkedHashMap;
-    }
-
 
 
 
